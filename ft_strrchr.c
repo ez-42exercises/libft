9dc22b1fc3b44e79313130208ea8360331f913bb
Ezequiel <gaonalarcon@gmail.com>
2025-03-04 20:45:07 +0100

    Fix null pointer initialization and improve character comparison in ft_strrchr function

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,16 +6,18 @@
 /*   By: pgaona-a <pgaona-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 00:31:01 by pgaona-a          #+#    #+#             */
-/*   Updated: 2025/03/04 20:21:46 by pgaona-a         ###   ########.fr       */
+/*   Updated: 2025/03/04 20:34:22 by pgaona-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 char	*ft_strrchr(const char *s, int c)
 {
 	int		i;
 	char	*found;
 
-	found = 0;
+	found = NULL;
 	i = 0;
 	while (s[i] != '\0')
 	{
@@ -25,7 +27,7 @@ char	*ft_strrchr(const char *s, int c)
 		}
 		i++;
 	}
-	if (c == '\0')
+	if ((unsigned char)c == '\0')
 		return ((char *)(s + i));
 	return (found);
 }
